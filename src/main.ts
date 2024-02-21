44ce740ea7356abda42323762958c7b2f6a0c452
Glenn Mark Flores <140369046+blackflare546@users.noreply.github.com>
2024-02-21 10:35:19 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+
+import { AppModule } from './app/app.module';
+
+runNativeScriptAngularApp({
+  appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
+});
