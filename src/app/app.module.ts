5fc1fa4539f1cb91831c67dabecb739ca77e01bc
Glenn Mark Flores <140369046+blackflare546@users.noreply.github.com>
2024-02-22 15:38:52 +0800

    feat: add material design ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
-
+import { NativeScriptMaterialCardViewModule } from "@nativescript-community/ui-material-cardview/angular";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, AppRoutingModule],
+  imports: [
+    NativeScriptModule,
+    AppRoutingModule,
+    NativeScriptMaterialCardViewModule,
+  ],
   declarations: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
--- src/app/modules/receipt/receipt.module.ts
+++ src/app/modules/receipt/receipt.module.ts
@@ -1,6 +1,5 @@
 import { NO_ERRORS_SCHEMA, NgModule } from "@angular/core";
 import { NativeScriptCommonModule } from "@nativescript/angular";
-
 import { ReceiptComponent } from "./receipt.component";
 import { ReceiptRoutingModule } from "./receipt-routing.module";
 
