e461cbbae25c53980ad831c6c785859c5a3f7d1c
Glenn Mark Flores <140369046+blackflare546@users.noreply.github.com>
2024-02-22 11:55:52 +0800

    feat: add base models

---
+++ src/app/core/models/components/app/base-app.component.d.ts
@@ -0,0 +1,17 @@
+import { BaseComponent } from "../base-component.model";
+
+export declare class BaseAppComponent {
+  constructor(...args: Array<any>) {
+    //
+  }
+
+  initialize(): void {
+    //
+  }
+  destroy(): void {
+    //
+  }
+  initializePush(): void {
+    //
+  }
+}
