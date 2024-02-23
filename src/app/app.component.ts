33af5ab8c231af8b0fde083970ee03c8c76fa07e
Glenn Mark Flores <140369046+blackflare546@users.noreply.github.com>
2024-02-23 14:21:21 +0800

    feat: update camera service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,4 +19,8 @@ export class AppComponent extends BaseAppComponent implements OnInit {
   takePhoto() {
     this._cameraService.capturePhoto();
   }
+
+  useCapturedImage() {
+    this._cameraService.getCapturedImage();
+  }
 }
--- src/app/shared/services/camera/camera.service.ts
+++ src/app/shared/services/camera/camera.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { requestPermissions } from "@nativescript/camera";
 import * as camera from "@nativescript/camera";
 import { Image } from "@nativescript/core";
+import { ImageSource, knownFolders, path } from "@nativescript/core";
 
 @Injectable({
   providedIn: "root",
@@ -14,18 +15,47 @@ export class CameraService {
   }
 
   capturePhoto(): void {
+    const options = {
+      width: 300,
+      height: 300,
+      keepAspectRatio: false,
+      saveToGallery: true,
+    };
+
     requestPermissions().then(() => {
       camera
-        .takePicture()
+        .takePicture(options)
         .then((imageAsset) => {
-          console.log("Result is an image asset instance");
-          this.capturedImage = new Image();
-          this.capturedImage.src = imageAsset;
+          console.log(
+            "Size: " +
+              imageAsset.options.width +
+              "x" +
+              imageAsset.options.height
+          );
+          console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
+          console.log(
+            "Photo saved in Photos/Gallery for Android or in Camera Roll for iOS"
+          );
+
+          ImageSource.fromAsset(imageAsset).then((imageSource: ImageSource) => {
+            const folderPath: string = knownFolders.documents().path;
+            const fileName: string = "test.jpg";
+            const filePath: string = path.join(folderPath, fileName);
+            const saved: boolean = imageSource.saveToFile(filePath, "jpg");
+
+            if (saved) {
+              console.log("Saved: " + filePath);
+              console.log("Image saved successfully!");
+            }
+          });
         })
         .catch((err) => {
           console.log("Error -> " + err.message);
         });
     });
-    console.log("CAMERA");
+  }
+
+  getCapturedImage() {
+    console.log("GetCapturedImage");
   }
 }
