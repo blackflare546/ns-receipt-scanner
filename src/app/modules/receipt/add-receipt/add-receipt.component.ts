622700f4521a7321ae7830526ae2975acc42a8ff
rtags911 <146685743+rtags911@users.noreply.github.com>
2024-02-27 16:24:36 +0800

    feat: added dynamic Text Fields

--- src/app/modules/receipt/add-receipt/add-receipt.component.ts
+++ src/app/modules/receipt/add-receipt/add-receipt.component.ts
@@ -1,15 +1,81 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, Injector, ViewContainerRef } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { BaseComponent } from "~/app/core/models/components/base-component.model";
+import {
+  SelectedIndexChangedEventData,
+  ValueList,
+} from "nativescript-drop-down";
+import { ReceiptService } from "~/app/shared/services";
 
 @Component({
   selector: "ns-add-receipt",
   templateUrl: "./add-receipt.component.html",
   styleUrls: ["./add-receipt.component.scss"],
 })
-export class AddReceiptComponent extends BaseComponent {
-  
+export class AddReceiptComponent extends BaseComponent implements OnInit {
+  form: FormGroup;
+  ngOnInit(): void {}
+  public selectedIndex = 1;
+  items: ValueList<string>;
+  public hint = "Select Category";
+  constructor(
+    private fb: FormBuilder,
+    protected injector: Injector,
+    private receiptService: ReceiptService
+  ) {
+    super(injector);
+    this.form = this.fb.group({
+      title: ["", Validators.required],
+      price: ["", Validators.required],
+      date: [new Date(), Validators.required],
+      notes: [""],
+      category: [""],
+    });
+
+    this.items = new ValueList<string>([
+      { value: "test1", display: "Test 1" },
+      { value: "test2", display: "Test 2" },
+      { value: "test3", display: "Test 3" },
+    ]);
+  }
+
+  onchange(args: SelectedIndexChangedEventData) {
+    console.log(
+      `Drop Down selected index changed from ${args.oldIndex} to ${args.newIndex}`
+    );
+
+    const selectedText = this.items.getDisplay(args.newIndex);
+    const selectedValue = this.items.getValue(args.newIndex);
+
+    console.log(`Selected text: ${selectedText}`);
+    console.log(`Selected value: ${selectedValue}`);
+  }
+
   back() {
     console.log("Test");
     this.navigate("/");
+  }
+
+  onSave() {
+    if (this.form.valid) {
+      const selectedCategoryText = this.items.getDisplay(
+        this.form.get("category").value
+      );
+
+      const receipt = {
+        title: this.form.get("title").value,
+        category: selectedCategoryText,
+        date: this.form.get("date").value,
+        price: this.form.get("price").value,
+      };
+
+      this.receiptService.addReceipt(receipt);
+      this.navigate("/");
+      console.log("Form data saved:", receipt);
+
+      this.form.reset();
+    } else {
+      console.log("Form data not saved. Validation errors:", this.form.value);
+    }
   }
 }
--- src/app/modules/receipt/add-receipt/add-receipt.module.ts
+++ src/app/modules/receipt/add-receipt/add-receipt.module.ts
@@ -1,12 +1,22 @@
+
 import { NO_ERRORS_SCHEMA, NgModule } from "@angular/core";
-import { NativeScriptCommonModule } from "@nativescript/angular";
+import {
+  NativeScriptCommonModule,
+  NativeScriptFormsModule,
+} from "@nativescript/angular";
 import { NativeScriptDateTimePickerModule } from "@nativescript/datetimepicker/angular";
-
 import { AddReceiptComponent } from "./add-receipt.component";
-
+import { ReactiveFormsModule } from "@angular/forms";
+import { DropDownModule } from "nativescript-drop-down/angular";
 @NgModule({
   declarations: [AddReceiptComponent],
-  imports: [NativeScriptCommonModule, NativeScriptDateTimePickerModule],
+  imports: [
+    NativeScriptCommonModule,
+    NativeScriptDateTimePickerModule,
+    NativeScriptFormsModule,
+    ReactiveFormsModule,
+    DropDownModule,
+  ],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class AddReceiptModule {}
