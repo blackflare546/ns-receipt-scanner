81b2982a94ff51490d494e5d2746cccb98147d8e
rtags911 <146685743+rtags911@users.noreply.github.com>
2024-02-28 17:41:45 +0800

    fix:1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from "@angular/core";
 
 import { BaseAppComponent } from "./core/models/components/app/base-app.component";
 import { CameraService } from "./shared/services";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "ns-app",
   templateUrl: "app.component.html",
 })
 export class AppComponent extends BaseAppComponent implements OnInit {
-  constructor(private _cameraService: CameraService) {
+  constructor(private _cameraService: CameraService, private router: Router) {
     super();
   }
 
@@ -16,12 +17,15 @@ export class AppComponent extends BaseAppComponent implements OnInit {
     // Init your component properties here.
   }
 
+  navigateToReport() {
+    this.router.navigate([{ outlets: { reportTab: ["report"] } }]);
+  }
+
   async takePhoto() {
     try {
       const image = await this._cameraService.capturePhoto();
-
+      this.navigateToReport();
       console.log("Photo captured:", image);
-      this._cameraService.navigateToReceipts();
     } catch (error) {
       console.error("Error capturing photo:", error);
     }
--- src/app/modules/receipt/receipt.component.ts
+++ src/app/modules/receipt/receipt.component.ts
@@ -1,31 +1,42 @@
-import { Component, Injector } from "@angular/core";
+import { Component, Injector, OnInit } from "@angular/core";
 import { BaseComponent } from "~/app/core/models/components/base-component.model";
+import { ReceiptService } from "~/app/shared/services";
 
 @Component({
   selector: "ns-receipt",
   templateUrl: "./receipt.component.html",
   styleUrls: ["./receipt.component.scss"],
 })
-export class ReceiptComponent extends BaseComponent {
+export class ReceiptComponent extends BaseComponent implements OnInit {
   receiptData: {
     title: string;
     category: string;
     date: Date;
     price: number;
-  }[] = [
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-    { title: "v1", category: "test", date: new Date(), price: 10000 },
-  ];
+  }[] = [];
 
+  constructor(
+    private receiptService: ReceiptService,
+    protected injector: Injector
+  ) {
+    super(injector);
+  }
+
+  totalAmount: number = 0;
   ngOnInit() {
     this.hideStatusBar();
+    this.receiptData = this.receiptService.getReceiptData();
+    this.calculateTotalAmount();
   }
   addReceipt() {
     this.navigate("add-receipt");
+  }
+
+  private calculateTotalAmount() {
+    this.totalAmount = this.receiptData.reduce((sum, item) => {
+      console.log("Item Price:", item.price);
+      return sum + Number(item.price);
+    }, 0);
+    console.log("Total Amount:", this.totalAmount);
   }
 }
